5fe365f04bfafa7cff932a83c76aa8edb01c5f27
surya <surya.sk05@outlook.com>
2020-08-17 11:03:04 +0530

    add UI framework

--- Leaderboard/Leaderboard.xaml.cs
+++ Leaderboard/Leaderboard.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Leaderboard
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Leaderboard : Page
     {
-        public MainPage()
+        public Leaderboard()
         {
             this.InitializeComponent();
-        }
-
-        private void HamburgerClick(object sender, RoutedEventArgs e)
-        {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
-        }
-
-        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
