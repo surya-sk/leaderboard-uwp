ffe2d82d8752206880882aa942b57bfedf3fdfb8
Surya <surya.sk05@outlook.com>
2020-08-29 16:02:23 +0530

    Add master/detail view

--- Leaderboard/MainPage.xaml.cs
+++ Leaderboard/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Leaderboard.Models;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,8 +24,12 @@ namespace Leaderboard
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Profile profile = Profile.GetInstance();
+        private ObservableCollection<Game> games;
         public MainPage()
         {
+            profile.ReadProfile();
+            games = profile.GetGamesList();
             this.InitializeComponent();
             ContentFrame.Navigate(typeof(Homepage));
         }
@@ -46,6 +52,11 @@ namespace Leaderboard
         private void NavigationViewItem_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
             ContentFrame.Navigate(typeof(Leaderboard));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
